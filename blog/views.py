35d6550a48af53b4192a1cf40944af721d2d0c59
vagrant <vagrant@precise32.(none)>
2015-07-16 17:58:11 +0000

    change views in blog

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home_page(request):
+    hel='<h1> Hello world<h1>'
+    return HttpResponse(hel)
+
+def indexmy(request):
+    context_mydict1 = {'title': "It's my first blog", 'boldmessage': "I am bold font from the context"}
+    return render(request, 'blog/indexmy.html', context_mydict1)
